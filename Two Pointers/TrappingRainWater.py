a392063e2edd7d47760f66f0f9ca54e47440d1ce
Justin Chen <jchen4086@gmail.com>
2025-01-02 19:46:46 -0500

    Create TrappingRainWater.py

---
+++ Two Pointers/TrappingRainWater.py
@@ -0,0 +1,25 @@
+# https://neetcode.io/problems/trapping-rain-water
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        res = 0
+        l, r = 0, len(height) - 1
+        while l < r:
+            lh, rh = height[l], height[r]
+            area = 0
+            if lh < rh:
+                ll = l + 1
+                while ll < r and height[ll] < lh:
+                    area += lh - height[ll]
+                    ll += 1
+                l = ll
+            else:
+                rr = r - 1
+                while rr > l and height[rr] < rh:
+                    area += rh - height[rr]
+                    rr -= 1
+                r = rr
+            res += area
+        return res
+            
+                
+                
