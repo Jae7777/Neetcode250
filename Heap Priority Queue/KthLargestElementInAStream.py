db4f9d6cd7390ab09f4ee6b76a50ab406c098a27
Justin Chen <jchen4086@gmail.com>
2025-01-02 20:29:40 -0500

    Create KthLargestElementInAStream.py

---
+++ Heap Priority Queue/KthLargestElementInAStream.py
@@ -0,0 +1,15 @@
+# https://neetcode.io/problems/kth-largest-integer-in-a-stream
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.pq = []
+        self.k = k
+        for n in nums:
+            self.add(n)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.pq, val)
+        if len(self.pq) > self.k:
+            heapq.heappop(self.pq)
+        print(self.pq)
+        return self.pq[0]
