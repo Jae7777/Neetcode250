1bf90937741b8763a1061c0dfb41c31bd57cef9c
Justin Chen <jchen4086@gmail.com>
2025-01-02 20:00:55 -0500

    Create LongestSubstringWithoutRepeatingCharacters.py

---
+++ Sliding Window/LongestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,16 @@
+# https://neetcode.io/problems/longest-substring-without-duplicates
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l, r = 0, 0
+        sset = set()
+        res = 0
+        while r < len(s):
+            lc, rc = s[l], s[r] 
+            if rc not in sset:
+                res = max(res, r - l + 1)
+                r += 1
+                sset.add(rc)
+            else:
+                sset.remove(lc)
+                l += 1
+        return res
