df2a6068a8302fc8088a21a937bd5506dd04f430
Justin Chen <jchen4086@gmail.com>
2025-01-02 20:19:26 -0500

    Create DesignAddAndSearchWordsDataStructure.py

---
+++ Tries/DesignAddAndSearchWordsDataStructure.py
@@ -0,0 +1,33 @@
+# https://neetcode.io/problems/design-word-search-data-structure
+class Node:
+    def __init__(self):
+        self.next = {}
+        self.isWord = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = Node()
+
+    def addWord(self, word: str) -> None:
+        d = self.root
+        for c in word:
+            if c not in d.next:
+                d.next[c] = Node()
+            d = d.next[c]
+        d.isWord = True
+
+    def search(self, word: str) -> bool:
+        def dfs(d, i):
+            if i == len(word):
+                return d.isWord
+            if word[i] == '.':
+                for c in d.next:
+                    if dfs(d.next[c], i + 1):
+                        return True
+            else:
+                if word[i] in d.next:
+                    return dfs(d.next[word[i]], i + 1)
+            return False
+        
+        return dfs(self.root, 0)
