4e909973e74ed712b72eb7d60219c4d1eb535489
Justin Chen <jchen4086@gmail.com>
2025-01-02 20:35:41 -0500

    Create NumberOfConnectedComponentsInAnUndirectedGraph.py

---
+++ Graphs/NumberOfConnectedComponentsInAnUndirectedGraph.py
@@ -0,0 +1,20 @@
+# https://neetcode.io/problems/count-connected-components
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        adjList = {u: [] for u in range(n)}
+        for u, v in edges:
+            adjList[u].append(v)
+            adjList[v].append(u)
+        
+        visited = set()
+        def visitCC(u):
+            visited.add(u)
+            for v in adjList[u]:
+                if v not in visited:
+                    visitCC(v)
+        nCC = 0
+        for u in range(n):
+            if u not in visited:
+                visitCC(u)
+                nCC += 1
+        return nCC
