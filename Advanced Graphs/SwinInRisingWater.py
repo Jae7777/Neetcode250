d21df182ac055857d9b6d546ee70636eebb6faef
Justin Chen <jchen4086@gmail.com>
2025-01-02 20:38:01 -0500

    Create SwinInRisingWater.py

---
+++ Advanced Graphs/SwinInRisingWater.py
@@ -0,0 +1,24 @@
+# https://neetcode.io/problems/swim-in-rising-water
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        ROW, COL = len(grid), len(grid[0])
+        pq = [(grid[0][0], 0, 0)]
+        minElevation = 0
+        visited = set()
+        while pq:
+            elevation, r, c = heapq.heappop(pq)
+            minElevation = max(minElevation, elevation)
+            visited.add((r, c))
+            if r == ROW - 1 and c == COL - 1:
+                break
+            if r - 1 >= 0 and (r - 1, c) not in visited:
+                heapq.heappush(pq, (grid[r - 1][c], r - 1, c))
+            if r + 1 < ROW and (r + 1, c) not in visited:
+                heapq.heappush(pq, (grid[r + 1][c], r + 1, c))
+            if c - 1 >= 0 and (r, c - 1) not in visited:
+                heapq.heappush(pq, (grid[r][c - 1], r, c - 1))
+            if c + 1 < COL and (r, c + 1) not in visited:
+                heapq.heappush(pq, (grid[r][c + 1], r, c + 1))
+            
+        return minElevation
+
