300074416dd580e33e2cf8830519e3c4a9ff7975
Justin Chen <jchen4086@gmail.com>
2025-01-02 19:52:37 -0500

    Create GenerateParentheses.py

---
+++ Stack/GenerateParentheses.py
@@ -0,0 +1,19 @@
+# https://neetcode.io/problems/generate-parentheses
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        stack = []
+        def populate(o, c):
+            if o == c == n:
+                res.append("".join(stack))
+                return
+            if o < n:
+                stack.append("(")
+                populate(o + 1, c)
+                stack.pop()
+            if c < o:
+                stack.append(')')
+                populate(o, c + 1)
+                stack.pop()
+        populate(0, 0)
+        return res
