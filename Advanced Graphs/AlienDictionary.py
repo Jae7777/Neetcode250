db2a74bdd03db2f710241785a7317704499e2ad1
Justin Chen <jchen4086@gmail.com>
2025-01-03 16:25:46 -0500

    Create AlienDictionary.py

---
+++ Advanced Graphs/AlienDictionary.py
@@ -0,0 +1,33 @@
+# https://neetcode.io/problems/foreign-dictionary
+class Solution:
+    def foreignDictionary(self, words: List[str]) -> str:
+        graph = {c: set() for w in words for c in w}
+        for i in range(len(words) - 1):
+            word1, word2 = words[i], words[i + 1]
+            for c1, c2 in zip(word1, word2):
+                if c1 == c2 and len(word1) > len(word2):
+                    return ""
+                if c1 != c2:
+                    graph[c1].add(c2)
+                    break
+        
+        res = []
+        visited = {}
+        def endTraverse(u):
+            if u in visited:
+                return visited[u]
+            visited[u] = True
+            if u in graph:
+                for v in graph[u]:
+                    if endTraverse(v):
+                        return True
+            visited[u] = False
+            res.append(u)
+            return False
+        
+        for u in graph:
+            print(u)
+            if endTraverse(u):
+                return ""
+
+        return "".join(res[::-1])
