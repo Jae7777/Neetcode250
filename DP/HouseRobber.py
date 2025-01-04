40afbc1b74f1f5250867c2c97ef5792e1a2f34e8
Justin Chen <jchen4086@gmail.com>
2025-01-04 17:15:54 -0500

    Create HouseRobber.py

---
+++ DP/HouseRobber.py
@@ -0,0 +1,11 @@
+# https://neetcode.io/problems/house-robber
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums[0], nums[1])
+        nums[2] = max(nums[0] + nums[2], nums[2])
+        for i in range(3, len(nums)):
+            nums[i] = max(nums[i] + nums[i - 2], nums[i] + nums[i - 3])
+        return max(nums[-1], nums[-2])
