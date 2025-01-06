e11e17c1f6dd6cf8c573a8211404031b3b18f60d
Justin Chen <jchen4086@gmail.com>
2025-01-05 19:04:12 -0500

    Create DecodeWays.py

---
+++ DP/DecodeWays.py
@@ -0,0 +1,14 @@
+# https://neetcode.io/problems/decode-ways
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        digit1 = set(['1', '2'])
+        digit2 = set(['0', '1', '2', '3', '4', '5', '6'])
+        cache = {len(s): 1} # initially one way to decode
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == '0':
+                cache[i] = 0
+            else:
+                cache[i] = cache[i + 1]
+            if i + 1 < len(s) and (s[i] == '1' or (s[i] in digit1 and s[i + 1] in digit2)):
+                cache[i] += cache[i + 2]
+        return cache[0]
