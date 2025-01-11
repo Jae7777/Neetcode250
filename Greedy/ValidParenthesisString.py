3f713742fee480d8a6438a391f3d39c565752c1f
Justin Chen <jchen4086@gmail.com>
2025-01-10 21:33:13 -0500

    Create ValidParenthesisString.py

---
+++ Greedy/ValidParenthesisString.py
@@ -0,0 +1,18 @@
+# https://neetcode.io/problems/valid-parenthesis-string
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        openMin, openMax = 0, 0
+        for c in s:
+            if c == '(':
+                openMin += 1
+                openMax += 1
+            if c == ')':
+                openMin -= 1
+                openMax -= 1
+            if c == '*':
+                openMin -= 1
+                openMax += 1
+            if openMax < 0:
+                return False
+            openMin = max(openMin, 0)
+        return openMin == 0
