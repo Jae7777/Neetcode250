d62c8c04a95f33479228d76381511ff0320761ae
Justin Chen <jchen4086@gmail.com>
2025-01-11 20:56:42 -0500

    Create DetectSquares.py

---
+++ Math & Geometry/DetectSquares.py
@@ -0,0 +1,21 @@
+# https://neetcode.io/problems/count-squares
+class CountSquares:
+
+    def __init__(self):
+        self.xyCount = defaultdict(int)
+        self.xy = []
+
+    def add(self, point: List[int]) -> None:
+        self.xyCount[(point[0], point[1])] += 1
+        self.xy.append(point)
+
+    def count(self, point: List[int]) -> int:
+        squares = 0
+        visited = set()
+        x_, y_ = point
+        for x, y in self.xy:
+            if (abs(x_ - x) != abs(y_ - y) or x_ == x or y_ == y):
+                continue
+            squares += self.xyCount[(x_, y)] * self.xyCount[(x, y_)]
+        return squares
+            
