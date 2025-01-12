aee8a85802bc5f504a1b282d429887f3a7fd22e1
Justin Chen <jchen4086@gmail.com>
2025-01-11 21:12:39 -0500

    Create SingleNumber.py

---
+++ Bit Manipulation/SingleNumber.py
@@ -0,0 +1,7 @@
+# https://neetcode.io/problems/single-number
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        for num in nums:
+            res ^= num
+        return res
