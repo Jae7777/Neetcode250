81df5a8c05a891d3b4dba60e715f64db3c7acc5d
Justin Chen <jchen4086@gmail.com>
2025-01-16 18:36:06 -0500

    Create RangeSumQuery2DImmutable.py

---
+++ Arrays & Hashing/RangeSumQuery2DImmutable.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/range-sum-query-2d-immutable/description/
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        ROW, COL = len(matrix), len(matrix[0])
+        self.sums = [[0 for _ in range(COL + 1)] for _ in range(ROW + 1)]
+        for r in range(1, ROW + 1):
+            for c in range(1, COL + 1):
+                self.sums[r][c] += self.sums[r-1][c] + self.sums[r][c-1] + matrix[r-1][c-1] - self.sums[r-1][c-1]
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        return self.sums[row2+1][col2+1] - self.sums[row2+1][col1] - self.sums[row1][col2+1] + self.sums[row1][col1]
+
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
