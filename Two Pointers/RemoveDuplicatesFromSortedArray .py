9137ec48ab57c3ca3f38c8071187301f19d63523
Justin Chen <jchen4086@gmail.com>
2025-01-18 15:22:20 -0500

    Create RemoveDuplicatesFromSortedArray .py

---
+++ Two Pointers/RemoveDuplicatesFromSortedArray .py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        prev = nums[0]
+        l, r = 1, 1
+        while r < len(nums):
+            if nums[r] > prev:
+                nums[l] = nums[r]
+                prev = nums[l]
+                l += 1
+                r += 1
+            else:
+                r += 1
+        return l
