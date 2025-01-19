8b6678387ffab02950a1d88f5c31b93a69a4fbd0
Justin Chen <jchen4086@gmail.com>
2025-01-18 21:30:42 -0500

    Create BoatsToSavePeople.py

---
+++ Two Pointers/BoatsToSavePeople.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/boats-to-save-people/
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        res = 0
+        l, r = 0, len(people) - 1
+        while l <= r:
+            if people[l] + people[r] <= limit:
+                l += 1
+            r -= 1
+            res += 1
+        return res
