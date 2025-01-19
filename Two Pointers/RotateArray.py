bed07ebb01d28226b69263f180c3b1d63c900e8e
Justin Chen <jchen4086@gmail.com>
2025-01-18 21:08:10 -0500

    Create RotateArray.py

---
+++ Two Pointers/RotateArray.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/rotate-array/
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        def flip(l, r):
+            while l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r -= 1
+        k %= len(nums)
+        flip(0, len(nums) - k - 1)
+        flip(len(nums) - k, len(nums) - 1)
+        flip(0, len(nums) - 1)
