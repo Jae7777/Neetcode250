18ac1f0070db5fea40e9ec93012b98229173a4da
Justin Chen <jchen4086@gmail.com>
2025-01-19 17:03:50 -0500

    Create AsteroidCollision.py

---
+++ Stack/AsteroidCollision.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/asteroid-collision/
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for ast in asteroids:
+            broken = False
+            while stack and ast <= 0 and stack[-1] > 0:
+                if stack[-1] < abs(ast):
+                    stack.pop()
+                elif stack[-1] == abs(ast):
+                    stack.pop()
+                    broken = True
+                    break
+                else:
+                    broken = True
+                    break
+            if not broken:
+                stack.append(ast)
+            
+        return stack
