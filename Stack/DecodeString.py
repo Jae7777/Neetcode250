2d4bf4c3dc5dc45ea93e25c303110f81882c884b
Justin Chen <jchen4086@gmail.com>
2025-01-19 19:15:26 -0500

    Create DecodeString.py

---
+++ Stack/DecodeString.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/decode-string/description/
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        for c in s:
+            if c != ']':
+                stack.append(c)
+            else:
+                token = deque()
+                while stack and stack[-1] != '[':
+                    token.appendleft(stack.pop())
+                stack.pop()
+                repeats = deque()
+                while stack and stack[-1].isdigit():
+                    repeats.appendleft(stack.pop())
+                for _ in range(int("".join(repeats))):
+                    stack.append("".join(token))
+        return "".join(stack)
