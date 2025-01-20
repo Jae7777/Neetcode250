196d37efb2438f035a8a05cabc242b58b59cf16c
Justin Chen <jchen4086@gmail.com>
2025-01-20 14:56:49 -0500

    algo correction

--- Binary Search/SearchInsertPosition.py
+++ Binary Search/SearchInsertPosition.py
@@ -6,9 +6,9 @@ class Solution:
         while l <= r:
             m = (l + r) // 2
             if nums[m] > target:
-                r -= 1
+                r = m - 1
             elif nums[m] < target:
-                l += 1
+                l = m + 1
             else:
                 return m
         return m + 1 if nums[m] < target else m
