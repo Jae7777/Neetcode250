04bca50a7c48fec5098e965dea55492e55885626
Justin Chen <jchen4086@gmail.com>
2025-02-15 14:30:14 +0800

    Create IPO.py

---
+++ Heap Priority Queue/IPO.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/ipo/
+class Solution:
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        capital_pq = []
+        profit_pq = []
+        for profit, capital in zip(profits, capital):
+            heapq.heappush(capital_pq, (capital, profit))
+        while k > 0:
+            while capital_pq and w >= capital_pq[0][0]:
+                _, profit = heapq.heappop(capital_pq)
+                heapq.heappush(profit_pq, -profit)
+            w += -heapq.heappop(profit_pq) if profit_pq else 0
+            k -= 1
+        return w
