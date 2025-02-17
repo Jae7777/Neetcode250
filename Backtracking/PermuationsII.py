b89e7be5d78cac2e0b6a49a270e18d3a6c6abeaa
Justin Chen <jchen4086@gmail.com>
2025-02-17 17:38:15 +0800

    Create PermuationsII.py

---
+++ Backtracking/PermuationsII.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/permutations-ii/
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        perm = []
+        count = defaultdict(int)
+        for num in nums:
+            count[num] += 1
+        def dfs():
+            if len(perm) == len(nums):
+                res.append(perm[:])
+                return
+            for num in count:
+                if count[num] > 0:
+                    count[num] -= 1
+                    perm.append(num)
+                    dfs()
+                    count[num] += 1
+                    perm.pop()
+        dfs()
+        return res
