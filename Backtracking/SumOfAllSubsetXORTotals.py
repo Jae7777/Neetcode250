bc675d21f86bfb4505c57e0b73442d1575f46e0a
Justin Chen <jchen4086@gmail.com>
2025-02-17 17:11:37 +0800

    Create SumOfAllSubsetXORTotals.py

---
+++ Backtracking/SumOfAllSubsetXORTotals.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/combinations/
+
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        res = 0
+        currSubset = []
+        def backtrack(i):
+            if i == len(nums):
+                nonlocal res
+                xorSum = 0
+                for num in currSubset:
+                    xorSum ^= num
+                res += xorSum
+                return
+            currSubset.append(nums[i])
+            backtrack(i + 1)
+            currSubset.pop()
+            backtrack(i + 1)
+        backtrack(0)
+        return res
