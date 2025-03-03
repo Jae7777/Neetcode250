f3556a1e293879e451ab00ebe330d7fabe0f6030
Justin Chen <jchen4086@gmail.com>
2025-03-03 18:16:45 -0500

    Create LemonadeChange.py

---
+++ Greedy/LemonadeChange.py
@@ -0,0 +1,48 @@
+# https://leetcode.com/problems/lemonade-change/
+# soo jank (beats 95.21% memory)
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        count = { 5: 0, 10: 0, 20: 0 }
+        for bill in bills:
+            change = bill - 5
+            if change == 0:
+                count[5] += 1
+            elif change == 5:
+                if count[5] == 0:
+                    return False
+                count[change] -= 1
+                count[bill] += 1
+            elif change == 15:
+                if (count[10] >= 1 and count[5] == 0) or (count[10] == 0 and count[5] < 3):
+                    return False
+                if count[10] > 0:
+                    count[10] -= 1
+                    count[5] -= 1
+                else:
+                    count[5] -= 3
+                count[20] += 1
+        return True
+
+# cleaner
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        count = { 5: 0, 10: 0}
+        for bill in bills:
+            change = bill - 5
+            if bill == 5:
+                count[5] += 1
+            elif bill == 10:
+                if count[5] >= 1:
+                    count[bill] += 1
+                    count[change] -= 1
+                else:
+                    return False
+            else:
+                if count[10] >= 1 and count[5] >= 1:
+                    count[10] -= 1
+                    count[5] -= 1
+                elif count[5] >= 3:
+                    count[5] -= 3
+                else:
+                    return False
+        return True
