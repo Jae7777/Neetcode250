83892833261957699f36e8f65a2beea03aa4e48a
Justin Chen <jchen4086@gmail.com>
2025-03-03 16:48:49 -0500

    Create StoneGame.py

---
+++ 2-D DP/StoneGame.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/stone-game/
+# dp
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        dp = {}
+        def dfs(l, r):
+            if l == r:
+                return piles[l]
+            if (l, r) in dp:
+                return dp[(l, r)]
+            res = 0
+            res = max(piles[l] - dfs(l + 1, r), piles[r] - dfs(l, r - 1))
+            dp[(l, r)] = res
+            return res
+        res = dfs(0, len(piles) - 1)
+        return True if res > 0 else False
+
+# it's mathematically solved given the constraints
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        return True
