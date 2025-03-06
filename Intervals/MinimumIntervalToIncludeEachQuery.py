b0032c87afd473da355994f36fa4534588b919ba
Justin Chen <jchen4086@gmail.com>
2025-03-06 11:10:45 -0500

    Update MinimumIntervalToIncludeEachQuery.py

--- Intervals/MinimumIntervalToIncludeEachQuery.py
+++ Intervals/MinimumIntervalToIncludeEachQuery.py
@@ -2,20 +2,18 @@
 # https://leetcode.com/problems/minimum-interval-to-include-each-query/
 
 from typing import List
-# O(n * m + k) time, O(n) space
-# where n is the difference of the max interval end and min interval start,
-# m is the number of intervals,
-# and k is the number of queries
+import heapq
 class Solution: 
     def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
-        numLine = [float('inf')] * (max([interval[1] for interval in intervals]) + 1)
-        for interval in intervals:
-            for i in range(interval[0], interval[1] + 1):
-                numLine[i] = min(numLine[i], interval[1] - interval[0] + 1)
-        res = []
-        for query in queries:
-            if query >= len(numLine) or numLine[query] == float('inf'):
-                res.append(-1)
-            else:
-                res.append(numLine[query])
-        return res
+        intervals.sort()
+        res = {}
+        i = 0
+        pq = []
+        for q in sorted(queries):
+            while i < len(intervals) and intervals[i][0] <= q:
+                heapq.heappush(pq, (intervals[i][1] - intervals[i][0] + 1, intervals[i][1]))
+                i += 1
+            while pq and pq[0][1] < q:
+                heapq.heappop(pq)
+            res[q] = pq[0][0] if pq else -1
+        return [res[q] for q in queries]
